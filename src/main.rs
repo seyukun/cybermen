e7395a5c07612eb64ee1ba0c3f995015f3bc7602
yus-sato@kalyte.ro <yushin-sato@kalytero.ne.jp>
2024-02-18 23:42:43 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -18,50 +18,43 @@ fn cmd(cmd: &str, args: &[&str]) {
 
 #[tokio::main(flavor = "multi_thread")]
 async fn main() {
-    // Listen address
-    let loc_addr = "0.0.0.0:0";
-
-    // Remote address
-    let rem_address = "163.43.185.240:3000";
-
-    // Create sockets
-    let socket = Arc::new(UdpSocket::bind(loc_addr).await.unwrap());
+    // Interface name
+    let iface: Arc<Iface>;
+    let if_addr: &str;
+    let if_name: &str = "cm0";
+    let loc_addr: &str = "0.0.0.0:0";
+    let rem_addr: &str = "163.43.185.240:3000";
+    let socket: Arc<UdpSocket> = Arc::new(UdpSocket::bind(loc_addr).await.unwrap());
+    let mut buf_if_addr = [0; 18];
 
     // Connect
-    match socket.connect(rem_address).await {
-        Ok(_) => {
-            println!("[ OK ] Connection established: {}", rem_address);
-        }
-        Err(e) => panic!("[ FAILED ] Connection unestablished: {}", e),
-    };
+    socket
+        .connect(rem_addr)
+        .await
+        .expect("[ FAILED ] Connection unestablished");
+    println!("[ OK ] Connection established: {}", rem_addr);
 
     // Send request
-    match socket.send(&[0; 1]).await {
-        Ok(_) => {
-            println!("[ OK ] SYN");
-        }
-        Err(e) => {
-            panic!("[ FAILED ] SYN cannot send: {}", e);
-        }
-    };
+    socket
+        .send(&[0; 1])
+        .await
+        .expect("[ FAILED ] SYN cannot send");
+    println!("[ OK ] SYN");
 
     // Get interface address
-    let mut if_addr = [0; 18];
-    let if_addr_len = match socket.recv(&mut if_addr).await {
-        Ok(f) => {
-            println!("[ OK ] ACK recived: {}", str::from_utf8(&if_addr).unwrap());
-            f
+    match socket.recv(&mut buf_if_addr).await {
+        Ok(len) => {
+            if_addr =
+                str::from_utf8(&buf_if_addr[..len]).expect("[ FAILED ] ACK packet is invalid");
         }
-        Err(e) => {
-            panic!("[ FAILED ] ACK dose not recived: {}", e);
-        }
-    };
-    let if_addr = str::from_utf8(&if_addr[..if_addr_len]).unwrap();
+        Err(e) => panic!("[ FAILED ] ACK dose not recived: {}", e),
+    }
+    println!("[ OK ] ACK recived: {}", if_addr);
 
     // Create a „local“ (kernel) endpoint.
-    let iface = Arc::new(Iface::new("cm0", Mode::Tap).unwrap());
-    #[rustfmt::skip] cmd("ip", &["addr", "add", "dev", iface.name(), if_addr]);
-    #[rustfmt::skip] cmd("ip", &["link", "set", "up", "dev", iface.name()]);
+    iface = Arc::new(Iface::new(if_name, Mode::Tun).unwrap());
+    cmd("ip", &["addr", "add", "dev", iface.name(), if_addr]);
+    cmd("ip", &["link", "set", "up", "dev", iface.name()]);
 
     let rsocket = socket.clone();
     let ssocket = socket.clone();
@@ -69,29 +62,33 @@ async fn main() {
     let siface = iface.clone();
 
     // Handling for receive packet
+    let runtime = Runtime::new().expect("[ FAILED ] Unable to create a runtime");
     let _ = thread::spawn(move || loop {
         let mut buf = [0; 1518];
-        let runtime = Runtime::new().expect("Unable to create a runtime");
-        let len = runtime.block_on(rsocket.recv(&mut buf)).unwrap();
-        println!("[ OK ] Recived: {}", len);
-        if len == 0 {
-            continue;
-        }
-        if len > 0 {
-            siface.send(&buf[..len]).unwrap();
-        }
+        match runtime.block_on(rsocket.recv(&mut buf)) {
+            Ok(len) => {
+                if len > 0 {
+                    siface.send(&buf[..len]).unwrap();
+                }
+            }
+            Err(e) => {
+                println!("[ FAILED ] Unable to block socket: {}", e);
+            }
+        };
     });
 
     // Handling for send packet
+    let runtime = Runtime::new().expect("[ FAILED ] Unable to create a runtime");
     let th_send = thread::spawn(move || loop {
         let mut buf = [0; 1518];
-        let len = match riface.recv(&mut buf) {
-            Ok(len) => len,
+        match riface.recv(&mut buf) {
+            Ok(len) => {
+                runtime
+                    .block_on(ssocket.send(&buf[..len]))
+                    .expect("[ FAILED ] Unable to block socket");
+            }
             Err(_) => continue,
         };
-        let runtime = Runtime::new().expect("Unable to create a runtime");
-        runtime.block_on(ssocket.send(&buf[..len])).unwrap();
-        println!("[ OK ] Send: {}", len);
     });
 
     // Keep alive until the thread is terminated by an error
--- src/modules/tuntap/src/tuntap.c
+++ src/modules/tuntap/src/tuntap.c
@@ -6,55 +6,55 @@
 #include <assert.h>
 #include <stdint.h>
 #include <string.h>
-
-#include <sys/socket.h>
-#ifdef __linux__
-#include <linux/if.h>
-#include <linux/if_tun.h>
-#else
-#include <net/if.h>
-#define IFF_TUN 0x0001
-#define IFF_TAP 0x0002
-#define IFF_NO_PI 0x1000
-#define TUNSETIFF _IOW('T', 202, int)
-#endif
 #include <sys/ioctl.h>
+#include <sys/socket.h>
+
+#ifdef __linux__
+# include <linux/if.h>
+# include <linux/if_tun.h>
+#else
+# include <net/if.h>
+# define IFF_TUN 0x0001
+# define IFF_TAP 0x0002
+# define IFF_NO_PI 0x1000
+# define TUNSETIFF _IOW('T', 202, int)
+#endif
 
 /**
- * fd ‒ the fd to turn into TUN or TAP.
- * name ‒ the name to use. If empty, kernel will assign something by itself.
+ * fd
+ * 	‒ the fd to turn into TUN or TAP.
+ * 
+ * name
+ * 	‒ the name to use. If empty, kernel will assign something by itself.
  *   Must be buffer with capacity at least 33.
- * mode ‒ 1 = TUN, 2 = TAP.
- * packet_info ‒ if packet info should be provided, if the given value is 0 it will not prepend packet info.
+ * 
+ * mode
+ * 	‒ 1 = TUN, 2 = TAP.
+ * 
+ * packet_info
+ * 	‒ if packet info should be provided, if the given value is 0 it will not prepend packet info.
  */
-int tuntap_setup(int fd, unsigned char *name, int mode, int packet_info) {
-	struct ifreq ifr;
-	memset(&ifr, 0, sizeof ifr);
-	switch (mode) {
-		case 1:
-			ifr.ifr_flags = IFF_TUN;
-			break;
-		case 2:
-			ifr.ifr_flags = IFF_TAP;
-			break;
-		default:
-			assert(0);
-	}
+int tuntap_setup(int fd, unsigned char *name, int mode, int packet_info)
+{
+	struct ifreq	ifr;
+	int				ioresult;
 
-	// If no packet info needs to be provided add corresponding flag
-	if (!packet_info) {
+	memset(&ifr, 0, sizeof(ifr));
+	if (mode == 1)
+		ifr.ifr_flags = IFF_TUN;
+	else if (mode == 2)
+		ifr.ifr_flags = IFF_TAP;
+	else
+		assert(0);
+	if (!packet_info)
+	{
 		ifr.ifr_flags |= IFF_NO_PI;
 	}
-
-	// Leave one for terminating '\0'. No idea if it is needed, didn't find
-	// it in the docs, but assuming the worst.
 	strncpy(ifr.ifr_name, (char *)name, IFNAMSIZ - 1);
-
-	int ioresult = ioctl(fd, TUNSETIFF, &ifr);
-	if (ioresult < 0) {
-		return ioresult;
-	}
+	ioresult = ioctl(fd, TUNSETIFF, &ifr);
+	if (ioresult < 0)
+		return (ioresult);
 	strncpy((char *)name, ifr.ifr_name, IFNAMSIZ < 32 ? IFNAMSIZ : 32);
 	name[32] = '\0';
-	return 0;
+	return (0);
 }
