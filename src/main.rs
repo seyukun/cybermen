dbefc5d205f5feee45094809c28582ba3640367d
yus-sato@kalyte.ro <yushin-sato@kalytero.ne.jp>
2024-02-19 01:16:47 +0900

    update

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
+use std::net::UdpSocket;
 use std::process::Command;
 use std::str;
 use std::sync::Arc;
 use std::{thread, time};
-use tokio::net::UdpSocket;
-use tokio::runtime::Runtime;
 use tuntap::{Iface, Mode};
 
 fn cmd(cmd: &str, args: &[&str]) {
@@ -24,25 +23,21 @@ async fn main() {
     let if_name: &str = "cm0";
     let loc_addr: &str = "0.0.0.0:0";
     let rem_addr: &str = "163.43.185.240:3000";
-    let socket: Arc<UdpSocket> = Arc::new(UdpSocket::bind(loc_addr).await.unwrap());
+    let socket: Arc<UdpSocket> = Arc::new(UdpSocket::bind(loc_addr).unwrap());
     let mut buf_if_addr = [0; 18];
 
     // Connect
     socket
         .connect(rem_addr)
-        .await
         .expect("[ FAILED ] Connection unestablished");
     println!("[ OK ] Connection established: {}", rem_addr);
 
     // Send request
-    socket
-        .send(&[0; 1])
-        .await
-        .expect("[ FAILED ] SYN cannot send");
+    socket.send(&[0; 1]).expect("[ FAILED ] SYN cannot send");
     println!("[ OK ] SYN");
 
     // Get interface address
-    match socket.recv(&mut buf_if_addr).await {
+    match socket.recv(&mut buf_if_addr) {
         Ok(len) => {
             if_addr =
                 str::from_utf8(&buf_if_addr[..len]).expect("[ FAILED ] ACK packet is invalid");
@@ -62,33 +57,38 @@ async fn main() {
     let siface = iface.clone();
 
     // Handling for receive packet
-    let runtime = Runtime::new().expect("[ FAILED ] Unable to create a runtime");
-    let _ = thread::spawn(move || loop {
-        let mut buf = [0; 1518];
-        match runtime.block_on(rsocket.recv(&mut buf)) {
-            Ok(len) => {
-                if len > 0 {
-                    siface.send(&buf[..len]).unwrap();
+    siface.set_non_blocking().unwrap();
+    let _ = tokio::spawn(async move {
+        loop {
+            let mut buf = [0; 1518];
+            match rsocket.recv(&mut buf) {
+                Ok(len) => {
+                    if len > 0 {
+                        siface.send(&buf[..len]).unwrap();
+                    }
                 }
-            }
-            Err(e) => {
-                println!("[ FAILED ] Unable to block socket: {}", e);
-            }
-        };
+                Err(e) => {
+                    println!("[ FAILED R ] Unable to block socket: {}", e);
+                }
+            };
+        }
     });
 
     // Handling for send packet
-    let runtime = Runtime::new().expect("[ FAILED ] Unable to create a runtime");
-    let th_send = thread::spawn(move || loop {
-        let mut buf = [0; 1518];
-        match riface.recv(&mut buf) {
-            Ok(len) => {
-                runtime
-                    .block_on(ssocket.send(&buf[..len]))
-                    .expect("[ FAILED ] Unable to block socket");
-            }
-            Err(_) => continue,
-        };
+    let th_send = tokio::spawn(async move {
+        loop {
+            let mut buf = [0; 1518];
+            match riface.recv(&mut buf) {
+                Ok(len) => {
+                    if len > 0 {
+                        ssocket
+                            .send(&buf[..len])
+                            .expect("[ FAILED S ] Unable to block socket");
+                    }
+                }
+                Err(_) => continue,
+            };
+        }
     });
 
     // Keep alive until the thread is terminated by an error
